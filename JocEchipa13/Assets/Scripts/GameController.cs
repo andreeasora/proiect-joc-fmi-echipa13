100e55dc05a087269b6426416ffe6291d687434d
Mihai <mihailazar269@gmail.com>
2023-01-08 22:32:30 +0200

    Added new enemy and enemy pool

--- JocEchipa13/Assets/Scripts/GameController.cs
+++ JocEchipa13/Assets/Scripts/GameController.cs
@@ -15,14 +15,19 @@ public class GameController : MonoBehaviour
         public BoxCollider2D Area;
         public float Chance;
     }
-    [SerializeField]
-    private EnemyPool enemyPool;
+    [Serializable]
+    public struct EnemyPoolChance {
+        public EnemyPool enemyPool;
+        public float Chance;
+    }
     [SerializeField]
     public static GameController Instance {get; private set;}
     [SerializeField]
     private float enemyRadius = 1.0f;
     [SerializeField]
     private EnemySpawnAreas[] enemySpawnAreas;
+    [SerializeField]
+    private EnemyPoolChance[] enemyPools;
     [SerializeField]
     private float enemySpawnWaveDelay = 5.0f;
     [SerializeField]
@@ -60,6 +65,7 @@ public class GameController : MonoBehaviour
 
     private void SpawnEnemy() {
         // functions places an enemy in a random position in a valid spawn area
+        EnemyPool enemyPool = RandomEnemyPool();
         Enemy enemy = enemyPool.Pool.Get();
         BoxCollider2D area = RandomAreaSelect();
 
@@ -91,5 +97,21 @@ public class GameController : MonoBehaviour
         }
 
         return enemySpawnAreas.First().Area;
+    }
+    private EnemyPool RandomEnemyPool() {
+        // Function returns a randomly selected area
+        var sum = enemyPools.Select(x => x.Chance).Sum();
+
+        var point = Random.value * sum;
+        var accumulator = 0.0f;
+
+        foreach (var pool in enemyPools)
+        {
+            accumulator += pool.Chance;
+            if (accumulator >= point)
+                return pool.enemyPool;
+        }
+
+        return enemyPools.First().enemyPool;
     }
 }
