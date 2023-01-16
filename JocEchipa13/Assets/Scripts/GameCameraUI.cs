2674c465c9885775908a9ddbecc18d353c54f280
ioan45 <alexandruculea45@gmail.com>
2023-01-17 01:28:33 +0200

    Added new weapon

--- JocEchipa13/Assets/Scripts/GameCameraUI.cs
+++ JocEchipa13/Assets/Scripts/GameCameraUI.cs
@@ -10,6 +10,9 @@ public class GameCameraUI : MonoBehaviour
     [SerializeField] TMP_Text playerScoreValueText;
     [SerializeField] Image[] hearts;
     [SerializeField] Sprite emptyHeartSprite;
+    [SerializeField] private Image usedWeapon;
+    [SerializeField] private TMP_Text ammoText;
+    [SerializeField] private Sprite weaponGreen, weaponRed;
 
     int lastFilledHeartIdx;
 
@@ -17,11 +20,36 @@ public class GameCameraUI : MonoBehaviour
     {
         lastFilledHeartIdx = Player.maxLives - 1;
         playerScoreValueText.text = "0";
+        
+        UpdateUsedWeapon();
+
         player.onScoreUpdate += UpdateScoreValue;
         player.onLifeLost += RemoveHeart;
+        player.onAmmoUpdate += UpdateAmmo;
+        player.onWeaponChange += UpdateUsedWeapon;
     }
 
     void RemoveHeart() => hearts[lastFilledHeartIdx--].sprite = emptyHeartSprite;
 
     void UpdateScoreValue() => playerScoreValueText.text = player.Score.ToString();
+
+    void UpdateUsedWeapon()
+    {
+        if (player.WeaponType == 1)
+        {
+            usedWeapon.sprite = weaponGreen;
+            ammoText.text = "INF";
+        }
+        else
+        {
+            usedWeapon.sprite = weaponRed;
+            ammoText.text = player.Weapon2Ammo.ToString() + " / " + Player.maxWeapon2Ammo.ToString();
+        }
+    }
+
+    void UpdateAmmo()
+    {
+        if (player.WeaponType == 2)
+            ammoText.text = player.Weapon2Ammo.ToString() + " / " + Player.maxWeapon2Ammo.ToString();
+    }
 }
